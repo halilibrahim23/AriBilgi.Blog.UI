16fda7cf2368bcb8dfab39d8c730176a703aa3d5
halilibrahim23 <haliliibrahim17@gmail.com>
2022-10-13 01:04:11 +0300

    Add project files.

---
+++ AriBilgi.Blog.UI/wwwroot/Admin/pages/articleAdd.js
@@ -0,0 +1,19 @@
+﻿ar app = angular.module('ArticleAddApp', []);
+
+app.controller("ArticleAddController", function ($scope, $http) {
+
+
+    $scope.CategoryAdd = function () {
+        $http({
+            method: "POST",
+            headers: { "Contet-Type": "Application/json;charset=utf-8" },
+            url: "https://localhost:44309/Api/Article/ArticleAdd",
+            data: $scope.article
+        }).then(function (d) {
+            alert(d.data.message[0]);
+        }, function (d) {
+            alert(d.data.errors.Title[0]);
+        });
+    }
+
+});
